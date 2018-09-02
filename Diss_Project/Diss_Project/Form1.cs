283bc6a30f2c20b9e3a9ea8fa860c5b6a9030926
VertexNo <novidmit@mail.ru>
2018-09-02 15:50:43 +0300

    Реализовал авторизацию. Нужно только допилить чтобы регистр учитывался

--- Diss_Project/Diss_Project/Form1.cs
+++ Diss_Project/Diss_Project/Form1.cs
@@ -21,29 +21,52 @@ namespace Diss_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            bool Connect = false;
             try
             {
-                ConnectWithDB().GetAwaiter();
+                ConnectWithDB().GetAwaiter(); //Проверка асинхронно подключения к БД
+                Connect = true; //Установили что подключение есть
+                
             }
             catch (Exception exp)
             {
-                MessageBox.Show("Нет подключения к базе данных!");
+                MessageBox.Show("Нет подключения к базе данных!"); //Лог
+            }
+            if (Connect) //Если есть подключение
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString)) //Подключаемся к базе
+                {                    
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(); //новая СКЛ команда
+                    command.CommandText = "SELECT * FROM Users where login ='"+textBox1.Text+"' and password ='"+textBox2.Text+"';";
+                    command.Connection = connection;
+                    SqlDataReader dr = command.ExecuteReader(); //Ридер для получения данных из селекта
+                    
+                    if (dr.HasRows) //если ридер нашел и вернул данные
+                    {
+                        Console.WriteLine("Авторизация пройдена");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Не удалось авторизоваться с введенными данными!");
+                    }
+                }
             }
             
         }
         private static async Task ConnectWithDB()
         {
             //string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=DissBD;Integrated Security=True";
-            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString; //брется из файла App.Config строка подключения к БД
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 await connection.OpenAsync();
                 Console.WriteLine("Подключение открыто");
-                System.Diagnostics.Debug.WriteLine("Подключение открыто");
             }
-            System.Diagnostics.Debug.WriteLine("Подключение закрыто");
             Console.WriteLine("Подключение закрыто...");
+
         }
     }
 }
