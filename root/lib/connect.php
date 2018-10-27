74026738b77cb100cc7f1d30ff83faa94c94754b
VertexNo <novidmit@mail.ru>
2018-10-27 16:53:42 +0300

    Работает регистрация и добавление пользователей в БД. Не сделал еще 2-ую часть, которая обрабатывает введенные данные

---
+++ root/lib/connect.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 027 27.10.18
+ * Time: 15:45
+ */
+@mysql_connect("localhost", "user", "user07001735")
+or die ("Error for connect to DB");
+@mysql_select_db("nodproject");
+?>
