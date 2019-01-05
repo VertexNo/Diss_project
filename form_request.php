e15ae97e6653c6062331ac56eec2e21e48caccc0
VertexNo <novidmit@mail.ru>
2019-01-05 19:39:29 +0300

    Начал делать форму обращений. Выводит сейчас ID обращения

--- form_request.php
+++ form_request.php
@@ -36,6 +36,63 @@ if(isset($_SESSION["email"]) && isset($_SESSION["password"]) /*&& ($_SESSION['fk
 
     <br><h1>Авторизованы</h1>
 
+    <!--/*Старт формы обращений*/-->
+    <?php //Option для выбора организации
+
+    $result_query_requests = $mysqli->query("
+select request_id,caption,short_description,date_create,IFNULL(date_resolve, 'Нет данных')
+, concat(userCreate.last_name,\" \",userCreate.first_name) as 'Создал', concat(userRespons.last_name, \" \",userCreate.first_name) as 'Ответственный',org.organisation_name,status.status_name,priority.priority_name,service.service_name
+from requests req
+inner join users userCreate on req.fk_create_user_id = userCreate.user_id
+inner join users userRespons on req.fk_responsible_user_id = userRespons.user_id
+inner join organisations org on org.id_organisation = userCreate.fk_organisation_id
+inner join status status on req.fk_status_id = status.status_id
+inner join priority priority on priority.priority_id = req.fk_priority_id
+inner join service service on req.fk_service_id = service.service_id
+where request_id > 0
+");
+    $result_query_num_requests = mysqli_num_rows($result_query_requests);
+    for ($i=0; $i <$result_query_num_requests; $i++)
+    {
+        $result = mysqli_fetch_array($result_query_requests);
+
+        echo 'RequestId = '.$result['request_id'];
+    ?>
+    <tr>
+        <td>
+            <input class="cellbut" type="submit" name="Requests_id" value=<?php echo $result['request_id']?>>
+        </td>
+
+    </tr>
+    <?php
+    }
+    ?>
+
+    <table>
+        <thead><!-- необязательный тег-->
+        <tr>
+            <th>Ячейка заголовка</th>
+            <th>Ячейка заголовка</th>
+        </tr>
+        </thead>
+        <tbody><!--необязательный тег-->
+        <tr>
+            <td>Ячейка данных</td>
+            <td>Ячейка данных</td>
+        </tr>
+        <tr>
+            <td>Ячейка данных</td>
+            <td>Ячейка данных</td>
+        </tr>
+        </tbody>
+    </table>
+
+
+    <!--Конец формы обращений -->
+
+
+
+
     <?php
     echo 'fk_Role_id'.$_SESSION['fk_Role_id'];
     echo 'user_id'.$_SESSION['user_id'];
